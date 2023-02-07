87558f9de3fc212b07977909e4fa2f5b1ebc5131
Ariful Islam <arifulislamjoy597.com>
2023-02-07 16:49:08 +0600

    base url change

--- src/redux/Feature/portfolio/api/apiSlice.js
+++ src/redux/Feature/portfolio/api/apiSlice.js
@@ -1,21 +1,21 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
-    reducerPath: "api",
-    baseQuery: fetchBaseQuery({
-        // baseUrl: "http://localhost:4000",
-        baseUrl: "https://my-site-backend-server-dfwv.onrender.com",
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    // baseUrl: "http://localhost:4000",
+    baseUrl: "https://my-site-backend-server-dfwv.onrender.com",
+  }),
+  endpoints: (builder) => ({
+    getPortfolios: builder.query({
+      query: () => "/feature-portfolio",
     }),
-    endpoints: (builder) => ({
-        getPortfolios: builder.query({
-            query: () => "/feature-portfolio",
-        }),
-        postUser: builder.mutation({
-            query: (data) => ({
-                url: "/users",
-                method: "POST",
-                body: data,
-            }),
-        }),
+    postUser: builder.mutation({
+      query: (data) => ({
+        url: "/users",
+        method: "POST",
+        body: data,
+      }),
     }),
+  }),
 });
 export const { useGetPortfoliosQuery, usePostUserMutation } = apiSlice;
